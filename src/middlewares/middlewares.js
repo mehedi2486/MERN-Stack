ffd81cffa01128a454c840811d50723eec8a51f4
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-02-28 21:08:15 +0600

    put midddleware in the get handler

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,25 +1,51 @@
 const express = require('express');
 const app = express();
 
+let requestCount = 0;
 
-function isEligibleForVote(req, res, next){
-   const age = req.query.age;
-     if (age >= 18){
-        next();
-     }else {
-        res.json(
-            { msg: "You are not eligible for voting"}
-        )
-     }
+
+function requestIncrease(req, res){
+requestCount = requestCount + 1;
+console.log(`Total number of requests = ${requestCount}`)
+
 }
+
+
+app.get('/sum',requestIncrease, function (req, res){
+   requestIncrease();
+   const a = parseInt(req.query.a);
+   const b = parseInt(req.query.b);
+   res.json({
+      ans: a+b
+   })
+})
+
+
+app.get('/multiply', function (req, res){
+   requestIncrease();
+   const a = parseInt(req.query.a);
+   const b = parseInt(req.query.b);
+   res.json({
+      ans: a*b
+   })
+})
+
+// app.get('/add/:a/:b', function(req, res){
+//    const a = parseInt(req.params.a);
+//    const b = parseInt(req.params.b);
+
+//    res.json({
+//       answer: a+b
+//    })
+// })
 
 // app.use(isEligibleForVote);
 
-app.get('/ride1', isEligibleForVote, function (req,res){
-   res.json ({
-      msg:"you have successfully riden the ride 1"
-   })
-})
+// app.get('/ride1', isEligibleForVote, function (req,res){
+//    res.json ({
+//       msg:"you have successfully riden the ride 1"
+//    })
+// })
 
 
 // app.get('/ride2', function (req, res){
