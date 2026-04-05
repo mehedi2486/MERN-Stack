e6ae74a1dcb5e639fb5b944985b725714d79114b
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-05 20:49:41 +0600

    add mongo and bcrypt

--- Basic_Mongo/index.js
+++ Basic_Mongo/index.js
@@ -1,102 +1,126 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { UserModel, TodoModel } = require("./db");
 const JWT_SECRET = "ajhdja93872948";
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const {z} = require("zod");
 
-mongoose.connect("mongodb+srv://admin:AELFeFNd6xKFuP0C@cluster0.rtlmthq.mongodb.net/todo-mehedi")
+mongoose.connect(
+  // 
+);
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
-    
-    res.json({
-        message: "You are signed up"
-    })
-});
+      const requireBody = z.object({
+        email:z.string().min(3).max(100).email(),
+        password:z.string().min(3).max(100),
+        name:z.string().min(3).max(100)
+      })
 
+      // const parsedData = requireBody.parse(req.body);
+      const parseDataWithSuccess = requireBody.safeParse(req.body);
 
-app.post("/signin", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const response = await UserModel.findOne({
-        email: email,
-        password: password,
-    });
-
-    if (response) {
-        const token = jwt.sign({
-            id: response._id.toString()
-        }, JWT_SECRET);
-
+      if(!parseDataWithSuccess.success){
         res.json({
-            token
+          message:"Incorrect format"
         })
-    } else {
-        res.status(403).json({
-            message: "Incorrect creds"
+        return
+
+      }
+
+
+        const email = req.body.email;
+        const password = req.body.password;
+        const name = req.body.name;
+
+    
+        const hasedPassword = await bcrypt.hash(password, 10);
+    
+        await UserModel.create({
+            email: email,
+            password: hasedPassword,
+            name: name
+        });
+        
+        res.json({
+            message: "You are signed up"
         })
-    }
+    });
+
+
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const response = await UserModel.findOne({
+    email: email,
+  });
+
+  const passwordMatch = await bcrypt.compare(password, response.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_SECRET,
+    );
+
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect creds",
+    });
+  }
 });
-
-
-
 
 function auth(req, res, next) {
-    const token = req.headers.token;
+  const token = req.headers.token;
 
-    const response = jwt.verify(token, JWT_SECRET);
+  const response = jwt.verify(token, JWT_SECRET);
 
-    if (response) {
-        req.userId = response.id;
-        next();
-    } else {
-        res.status(403).json({
-            message: "Incorrect creds"
-        })
-    }
+  if (response) {
+    req.userId = response.id;
+    next();
+  } else {
+    res.status(403).json({
+      message: "Incorrect creds",
+    });
+  }
 }
 
+app.post("/todo", auth, async function (req, res) {
+  const userId = req.userId;
+  const title = req.body.title;
+  const done = req.body.done;
 
+  await TodoModel.create({
+    userId,
+    title,
+    done,
+  });
 
-app.post("/todo", auth, async function(req, res) {
-    const userId = req.userId;
-    const title = req.body.title;
-    const done = req.body.done;
-
-    await TodoModel.create({
-        userId,
-        title,
-        done
-    });
-
-    res.json({
-        message: "Todo created"
-    })
+  res.json({
+    message: "Todo created",
+  });
 });
 
+app.get("/todos", auth, async function (req, res) {
+  const userId = req.userId;
 
-app.get("/todos", auth, async function(req, res) {
-    const userId = req.userId;
+  const todos = await TodoModel.find({
+    userId,
+  });
 
-    const todos = await TodoModel.find({
-        userId
-    });
-
-    res.json({
-        todos
-    })
+  res.json({
+    todos,
+  });
 });
 
 app.listen(3000);
